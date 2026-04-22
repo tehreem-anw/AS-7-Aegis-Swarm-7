bf4a4ad2fe5cbea18102eb6b8f4e89b7a05d499c
Tehreem Anwar <tehreemhb@gmail.com>
2026-04-22 04:36:07 -0400

    Threat Abstract Base Class
    Defines the template for all threats using polymorphism. Implements composition with Coordinate and TargetProfile classes, and establishes a pure virtual trajectory logic.

---
+++ include/Threat.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Coordinate.h"
+#include "TargetProfile.h"
+
+using namespace std;
+
+// Abstract Base Class
+class Threat {
+protected:
+    string threatID;
+    Coordinate currentPosition;		//Composition: Each target has a current position
+    TargetProfile profile; 			//Composition: Each target has a target profile
+    double speed;
+
+public:
+    // Constructor
+    Threat(string id, Coordinate pos, TargetProfile prof, double s);
+
+    // Virtual Destructor
+    virtual ~Threat() {}
+
+    // Pure Virtual Function: Each threat moves according to different physics
+    virtual void calculateTrajectory() = 0;
+
+    // Shared display logic
+    virtual void displayThreatInfo() const;
+
+    // Getters
+    string getID() const { return threatID; }
+    Coordinate getPosition() const { return currentPosition; }
+    TargetProfile getProfile() const { return profile; }
+    double getSpeed() const { return speed; }
+};
