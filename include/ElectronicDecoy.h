1a1c7e1ae5ba37c4e6ed3b14213134b472e43b70
Tehreem Anwar <tehreemhb@gmail.com>
2026-04-22 10:44:38 -0400

    Update ElectronicDecoy.h

--- include/ElectronicDecoy.h
+++ include/ElectronicDecoy.h
@@ -4,6 +4,7 @@ using namespace std;
 
 class ElectronicDecoy : public Threat {
 public:
+    // Calls base Threat constructor
     ElectronicDecoy(string id, Coordinate pos, TargetProfile prof, double s);
 
     // Implements simple straight-line movement
