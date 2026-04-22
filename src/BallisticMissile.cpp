0d70fd46ae1ee8b7218eaf8c4bdb2915dcd2f698
Tehreem Anwar <tehreemhb@gmail.com>
2026-04-22 07:22:57 -0400

    Update BallisticMissile.cpp

--- src/BallisticMissile.cpp
+++ src/BallisticMissile.cpp
@@ -4,7 +4,7 @@ using namespace std;
 BallisticMissile::BallisticMissile(string id, Coordinate pos, TargetProfile prof, double s): Threat(id, pos, prof, s) {}
 
 void BallisticMissile::calculateTrajectory() {
-    // Moves rapidly towards origin in a parabolic arc: X decreases, Y follows parabolic drop
+    // Moves rapidly towards origin in a parabolic arc: X decreases, Y follows parabolic path
     double x = currentPosition.getX() - (speed * 0.1);
     double y = currentPosition.getY() - (0.5 * 9.8 * 0.1 * 0.1); // Simple gravity effect
     currentPosition = Coordinate(x, y, currentPosition.getZ());
