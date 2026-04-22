4b9bbda64a91ab3a72412dc60c3793ece6437e30
Tehreem Anwar <tehreemhb@gmail.com>
2026-04-22 13:26:45 -0400

    SniperDrone Derived Class
    Implements stationary precision defense behavior. Overrides movement logic to maintain a fixed coordinate while providing instant-intercept capabilities for the last line of surface defense.

---
+++ include/SniperDrone.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Interceptor.h"
+using namespace std;
+
+// Derived class for stationary precision defense
+class SniperDrone : public Interceptor {
+public:
+    // Calls base Interceptor constructor
+    SniperDrone(string id, Coordinate pos, double s);
+
+    // Overrides updatePosition; note that this unit is stationary
+    void updatePosition() override;
+};
