c754cfd7b3c82faf2948bfa1120855f38b4acf26
Tehreem Anwar <tehreemhb@gmail.com>
2026-04-22 12:43:57 -0400

    JammerDrone Derived Class
    Implements electronic counter-measure (ECM) logic. Designed for "soft-kill" neutralization by maintaining proximity to electronic decoys and disrupting their signal without any physical impact.

---
+++ include/JammerDrone.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Interceptor.h"
+using namespace std;
+
+// Derived class for mobile electronic interception
+class JammerDrone : public Interceptor {
+public:
+    // Calls base Interceptor constructor
+    JammerDrone(string id, Coordinate pos, double s);
+
+    // Overrides updatePosition to implement jamming proximity logic
+    void updatePosition() override;
+};
