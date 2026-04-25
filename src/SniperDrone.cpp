114664c39ac12f6547e1f191bbeb54c7c7e46394
Tehreem Anwar <tehreemhb@gmail.com>
2026-04-25 05:55:27 -0400

    Update SniperDrone.cpp

--- src/SniperDrone.cpp
+++ src/SniperDrone.cpp
@@ -4,6 +4,8 @@ using namespace std;
 // Parametrized Constructor
 SniperDrone::SniperDrone(string id, Coordinate pos, double s): Interceptor(id, pos, s) {}
 
+bool isExpendable() const override { return false; } // Shoots laser, survives
+
 // Implements behavior for the stationary sniper unit
 void SniperDrone::updatePosition() { 
     // This is a stationary unit. Position remains fixed while monitoring its engagement range.
