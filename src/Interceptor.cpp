fc7595049a16b9f10d0ff65076c3df441f8cb12c
Tehreem Anwar <tehreemhb@gmail.com>
2026-04-25 05:37:39 -0400

    Update Interceptor.cpp

--- src/Interceptor.cpp
+++ src/Interceptor.cpp
@@ -1,4 +1,6 @@
 #include "Interceptor.h"
+#include "IntelMessage.h"
+#include "CommandCenter.h"
 using namespace std;
 
 // Parametrized Constructor: Initializes identity, position, and active status
@@ -11,4 +13,15 @@ void Interceptor::displayInterceptorInfo() const {
     cout<<"Current Position: ";
     currentPosition.display();
     cout<<endl;
+}
+
+// Base implementation: Most drones just log the message
+void Interceptor::receiveMessage(const IntelMessage& msg) {
+    cout<<"[SYSTEM]: Drone "<<unitID<<" received packet from "<<msg.getSender()<<endl;
+}
+
+// Drones can send status updates back to the Command Center
+void Interceptor::sendMessage(IntelMessage& msg, CommandCenter& center) {
+    cout<<"[COMMS]: Drone "<<unitID<<" transmitting data to Command Center."<<endl;
+    center.processIncomingIntel(msg); 	// Logic for the Command Center to handle data
 }
